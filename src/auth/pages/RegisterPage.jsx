bca745105a15d3e1cb157d22298b90cc8ed23255
Victor-Venegas-Calbillan <victort8012@gmail.com>
2023-01-20 15:47:24 -0300

    Agregando validaciones al formulario de registro

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,60 +1,106 @@
-import { Link as RouterLink } from 'react-router-dom'
-import {
-  Button,
-  Grid,
-  TextField,
-  Typography,
-  Link
-} from '@mui/material';
+import { useState } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import { Button, Grid, TextField, Typography, Link } from '@mui/material';
 import { AuthLayout } from '../layout/AuthLayout';
+import { useForm } from '../../hooks';
+
+const formData = {
+  displayName: '',
+  email: '',
+  password: '',
+};
+
+const formValidation = {
+  email: [(value) => value.includes('@'), 'el correo debe tener un @.'],
+  password: [(value) => value.length > 5, 'la contraseña debe tener mas de 5 caracteres.'],
+  displayName: [(value) => value.length > 5, 'el nombre debe tener mas de 5 caracteres.'],
+}
 
 export const RegisterPage = () => {
+
+  const [formSubmitted, setFormSubmitted] = useState(false)
+
+  const {
+    displayName,
+    email,
+    password,
+    onInputChange,
+    displayNameValid,
+    emailValid,
+    passwordValid,
+    isFormValid
+  } = useForm(formData, formValidation);
+
+
+  const onSubmit = ( e ) => {
+    e.preventDefault();
+    setFormSubmitted(true);
+  }
+
   return (
     <AuthLayout title='Register'>
-      <form>
+      <h1>Form Valid: { isFormValid ? 'valido' : 'no valido' }</h1>
+      <form onSubmit={onSubmit}>
         <Grid container>
-          <Grid item xs={12} sx={{ mt: 2 }}>
-            <TextField 
-              label='Nombre completo' 
-              type='email' 
+          <Grid item xs={ 12 } sx={{ mt: 2 }}>
+            <TextField
+              label='Nombre completo'
+              type='text'
               fullWidth
               placeholder='Tu nombre completo'
+              name='displayName'
+              value={ displayName }
+              onChange={ onInputChange }
+              error={ !!displayNameValid && formSubmitted }
+              helperText={ displayNameValid }
             />
           </Grid>
-          <Grid item xs={12} sx={{ mt: 2 }}>
-            <TextField 
-              label='Correo' 
-              type='email' 
+          <Grid item xs={ 12 } sx={{ mt: 2 }}>
+            <TextField
+              label='Correo'
+              type='email'
               fullWidth
               placeholder='correo@correo.com'
+              name='email'
+              value={ email }
+              onChange={ onInputChange }
+              error={ !!emailValid && formSubmitted }
+              helperText={ emailValid }
             />
           </Grid>
-          <Grid item xs={12} sx={{ mt: 2 }}>
+          <Grid item xs={ 12 } sx={{ mt: 2 }}>
             <TextField
               label='Contraseña'
               type='Password'
               fullWidth
               placeholder='contraseña'
+              name='password'
+              value={ password }
+              onChange={ onInputChange }
+              error={ !!passwordValid && formSubmitted }
+              helperText={ passwordValid }
             />
           </Grid>
-          <Grid item xs={12} sx={{ mt: 2 }}>
-            <TextField
-              label='Repiter contraseña'
-              type='Password'
-              fullWidth
-              placeholder='confirmar contraseña'
-            />
-          </Grid>
-          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
-            <Grid item xs={12}>
-              <Button variant='contained' fullWidth>
+          <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={ 12 }>
+              <Button 
+                variant='contained' 
+                fullWidth
+                type='submit'
+              >
                 Crear Cuenta
               </Button>
             </Grid>
           </Grid>
-          <Grid container direction="row" justifyContent="end">
-            <Typography sx={{ mr:1 }}>¿Ya tienes cuenta?</Typography>
-            <Link component={ RouterLink } color="inherit" to="/auth/login">
+          <Grid container direction='row' justifyContent='end'>
+            <Typography sx={{ mr: 1 }}>
+              ¿Ya tienes cuenta?
+            </Typography>
+            <Link
+              component={RouterLink}
+              color='inherit'
+              to='/auth/login'
+            >
               Ingresar
             </Link>
           </Grid>
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,11 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
-export const useForm = (initialState = {}) => {
-  const [fromValues, setFromValues] = useState(initialState);
+export const useForm = (initialState = {}, formValidations) => {
+  const [formValues, setFromValues] = useState(initialState);
+  const [formValidation, setFromValidation] = useState({});
+
+  useEffect(() => {
+    createValidators();
+  }, [formValues])
+
+  const isFormValid = useMemo(() => {
+
+    for (const formValue of Object.keys(formValidation)) {
+      if( formValidation[formValue] !== null ) return false
+    }
+
+    return true;
+  }, [formValidation])
+  
 
   const onInputChange = ({ target }) => {
     setFromValues({
-      ...fromValues,
+      ...formValues,
       [target.name]: target.value,
     });
   }
@@ -14,10 +29,24 @@ export const useForm = (initialState = {}) => {
     setFromValues(initialState);
   }
 
+  const createValidators = () => {
+    const formCheckdValues = {};
+
+    for (const formField of Object.keys( formValidations )) {
+      const [fn , errorMessage = 'Este campo es requerido.'] = formValidations[formField]; 
+
+      formCheckdValues[`${ formField }Valid`] = fn( formValues[formField] ) ? null : errorMessage;
+
+    }
+    setFromValidation( formCheckdValues );
+  }
+
   return {
-    ...fromValues,
+    ...formValues,
     onInputChange,
     onReset,
-    fromValues
+    fromValues: formValues,
+    ...formValidation,
+    isFormValid
   }
 };
