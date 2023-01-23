856c80e87d0fdf49a84b665f95abedbb220375b9
Victor-Venegas-Calbillan <victort8012@gmail.com>
2023-01-23 18:40:27 -0300

    Creando rutas condicionales para los usuarios

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,29 @@
-import { useSelector } from "react-redux"
-import { Route, Routes } from "react-router-dom"
+import { onAuthStateChanged } from "firebase/auth"
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { Navigate, Route, Routes } from "react-router-dom"
 import { AuthRoutes } from "../auth/routes/AuthRoutes"
+import { firebaseAuth } from "../firebase/config"
 import { JournalRoutes } from "../journal/routes/JournalRoutes"
+import { login, logOut } from "../store/auth"
 import { ChekingAuth } from "../ui"
 
 export const AppRouter = () => {
 
   const { status } = useSelector( state => state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged( firebaseAuth, async (user) => {
+      if (!user) return dispatch(logOut);
+
+      const {uid, email, displayName, photoURL } = user
+
+      dispatch ( login({ uid, email, displayName, photoURL }) )
+    } );
+    
+  }, [])
+  
 
   if( status === 'checking' ) {
     return <ChekingAuth />
@@ -14,8 +31,13 @@ export const AppRouter = () => {
   
   return (
     <Routes>
-      <Route path="/auth/*" element={<AuthRoutes />} />
-      <Route path="/*" element={<JournalRoutes />} />
+      {
+        (status === 'authenticated')
+        ? <Route path="/*" element={<JournalRoutes />} />
+        : <Route path="/auth/*" element={<AuthRoutes />} />
+      }
+
+      <Route path="/*" element={<Navigate to="/auth/login" />} />
     </Routes>
   )
 }
