65828d7031f71fc7398550cbeafc23c72809c300
Victor-Venegas-Calbillan <victort8012@gmail.com>
2023-01-23 18:27:29 -0300

    Creando componente de load mientras esta en el estado de checking

---
+++ src/ui/components/ChekingAuth.jsx
@@ -0,0 +1,20 @@
+import { CircularProgress, Grid } from '@mui/material'
+
+export const ChekingAuth = () => {
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ minHeight: "100vh", backgroundColor: 'primary.main', padding: 4 }}  
+    >
+      <Grid
+        item
+      >
+        <CircularProgress color='error' size={100} />
+      </Grid>
+    </Grid>
+  )
+}
